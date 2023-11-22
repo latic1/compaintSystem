affa99eb088a3eaf4cc929526473d560a855247a
latic1 <laticlatif2@gmail.com>
2023-11-22 13:41:05 +0000

    cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
+import  cors from 'cors';
+
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import userRoutes from './routes/userRoutes.js';
 
@@ -16,6 +18,8 @@ const port = process.env.PORT || 5000;
 
 // Create an Express application
 const app = express();
+app.use(cors());
+
 
 // Middleware for parsing JSON and URL-encoded request bodies
 app.use(express.json());
